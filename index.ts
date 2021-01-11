e22006f9fe922333a8705159cab060d79cbe8a87
Daichi Kudo <sakanactor624@gmail.com>
2021-01-11 10:08:43 +0900

    :sparkles: domの状態管理でのtodo追加機能

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 import { App } from "./src/App";
 const app = new App();
+app.mount();
--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,20 @@
-console.log("App.js: loaded");
+import { element } from "./view/html-util";
 
 export class App {
-  constructor() {
-    console.log("App initialized");
+  mount() {
+    const formElement = document.getElementById("js-form");
+    const inputElement = <HTMLInputElement>document.getElementById("js-form-input");
+    const containerElement = document.getElementById("js-todo-list");
+    const todoItemCountElement = document.getElementById("js-todo-count");
+
+    let todoItemCount = 0;
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      containerElement.appendChild(todoItemElement);
+      todoItemCount++;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      inputElement.value = "";
+    });
   }
 }
